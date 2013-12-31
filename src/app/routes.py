9be3c137ba12d3944820efce6696da1ba79e87a4
Mark Pashby <markpashby@gmail.com>
2013-12-31 12:26:16 +0000

    Got CPU Load working with JSONify

---
+++ src/app/routes.py
@@ -0,0 +1,24 @@
+import psutil
+from flask import Flask, jsonify, render_template
+ 
+app = Flask(__name__)      
+ 
+@app.route('/')
+def home():
+  return render_template('home.html')
+
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
+@app.route('/projects')
+def projects():
+  return render_template('projects.html')
+
+@app.route('/_stuff', methods= ['GET'])
+def stuff():
+    cpu=psutil.cpu_percent()
+    return jsonify(cpu=cpu)
+ 
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
