abfa4049a6ce183a0938ba7c3eeb206855eec06c
Mark Pashby <mark.pashby@imagination.com>
2015-03-17 16:24:15 +0000

    changes to some elements including adding settimeout feature

--- src/app/static/js/ulive.js
+++ src/app/static/js/ulive.js
@@ -1,22 +1,18 @@
-
-(function($) {
-    // Ulive CPU Function - Checks CPU data from dummy url        
-    
 var usageMethods = {
-    cpu: function() {
-   
+
+	cpu:	function() {
+
             $.getJSON("../_stuff",
                 function(data) {
                     var cpuPercent = data.cpu+"%"
                     $("#cpuload").text(cpuPercent);
-                    $("#barwidth").css('width', cpuPercent);    
+                    $("#barwidth").css('width', cpuPercent);
                 });
+		}
 
-        }
     };
 
-
-    setInterval(usageMethods.cpu, 2000);
-    usageMethods.cpu()
-  
-})(jQuery);
+(function callJSON() {
+	usageMethods.cpu();
+    setTimeout(	callJSON, 500);
+})();
